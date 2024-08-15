107b1e66b0eec628b1abda40b48b553312d0c9b9
yashmangal05 <yashmangal05@gmail.com>
2024-08-16 03:12:52 +0530

    Endpoint Security

--- src/main/java/com/example/ecommerce_backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/example/ecommerce_backend/api/security/WebSecurityConfig.java
@@ -4,14 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable().cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests()
+                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                .anyRequest()
+                .authenticated();
         return http.build();
     }
 }
