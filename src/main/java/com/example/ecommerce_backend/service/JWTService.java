b11436d31a937407791dc8f032dcff16b7570622
yashmangal05 <yashmangal05@gmail.com>
2024-08-21 00:34:59 +0530

    Denying fake JWT tokens

--- src/main/java/com/example/ecommerce_backend/service/JWTService.java
+++ src/main/java/com/example/ecommerce_backend/service/JWTService.java
@@ -2,6 +2,7 @@ package com.example.ecommerce_backend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.ecommerce_backend.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -44,6 +45,7 @@ public class JWTService {
     }
 
     public String getUsername(String token){
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+        DecodedJWT jwt = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return jwt.getClaim(USERNAME_KEY).asString();
     }
 }
--- src/test/java/com/example/ecommerce_backend/service/JWTServiceTest.java
+++ src/test/java/com/example/ecommerce_backend/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.ecommerce_backend.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.example.ecommerce_backend.model.LocalUser;
 import com.example.ecommerce_backend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +17,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @AutoConfigureMockMvc
 public class JWTServiceTest {
 
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
     @Autowired
     private JWTService jwtService;
     @Autowired
@@ -30,5 +37,20 @@ public class JWTServiceTest {
         LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
         String token = jwtService.generateJWT(user);
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
+    }
+
+    @Test
+    public void testJWTNOTGeneratedByUs(){
+        String token = JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                "NotTheRealSecret"
+        ));
+        Assertions.assertThrows(SignatureVerificationException.class, () -> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer(){
+        String token = JWT.create().withClaim("USERNAME", "UserA")
+                .sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class, () -> jwtService.getUsername(token));
     }
 }
